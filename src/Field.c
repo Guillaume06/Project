74506d47433f945e90cc9d5e9f31c6b8e30e88a2
guillaume <guillaume_fdr@orange.fr>
2016-09-17 16:00:36 +0200

    Collision detection

--- src/Field.c
+++ src/Field.c
@@ -17,7 +17,7 @@ Field f;
 // Movement functions
 
 // 0 for delete, 1 for add
-void EntityMod(Entity e, int mod){
+void entityMod(Entity e, int mod){
     for (int i = 0; i <= 3; i++){
         f[e.x+i][e.y] = mod;
         f[e.x][e.y+i] = mod;
@@ -33,6 +33,69 @@ void EntityMod(Entity e, int mod){
     f[e.x+2][e.y+3] = mod;
 }
 
+// 0 if ok, 1 if not
+int checkMoveUp(Entity e){
+    int ret = 0;
+    if ((f[e.x][e.y+4] != 0) && (f[e.x][e.y+4] != 3)) ret = 1;
+    if ((f[e.x+1][e.y+4] != 0) && (f[e.x+1][e.y+4] != 3)) ret = 1;
+    if ((f[e.x+2][e.y+4] != 0) && (f[e.x+2][e.y+4] != 3)) ret = 1;
+    if ((f[e.x+3][e.y+4] != 0) && (f[e.x+3][e.y+4] != 3)) ret = 1;
+    return ret;
+}
+
+// 0 if ok, 1 if not
+
+int checkMoveUpperLeft(Entity e){
+    int ret = 0;
+    if ((f[e.x][e.y+4] != 0) && (f[e.x][e.y+4] != 3)) ret = 1;
+    if ((f[e.x+1][e.y+4] != 0) && (f[e.x+1][e.y+4] != 3)) ret = 1;
+    if ((f[e.x+2][e.y+4] != 0) && (f[e.x+2][e.y+4] != 3)) ret = 1;
+
+    if ((f[e.x-1][e.y] != 0) && (f[e.x-1][e.y] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+1] != 0) && (f[e.x-1][e.y+1] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+2] != 0) && (f[e.x-1][e.y+2] != 3)) ret = 1;
+
+    if ((f[e.x-1][e.y+4] != 0) && (f[e.x-1][e.y+4] != 3)) ret = 1;
+    return ret;
+}
+
+// 0 if ok, 1 if not
+
+int checkMoveLeft(Entity e){
+    int ret = 0;
+    if ((f[e.x-1][e.y] != 0) && (f[e.x-1][e.y] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+1] != 0) && (f[e.x-1][e.y+1] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+2] != 0) && (f[e.x-1][e.y+2] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+3] != 0) && (f[e.x-1][e.y+3] != 3)) ret = 1;
+    return ret;
+}
+
+// 0 if ok, 1 if not
+
+int checkMoveLowerLeft(Entity e){
+    int ret = 0;
+    if ((f[e.x][e.y-1] != 0) && (f[e.x][e.y-1] != 3)) ret = 1;
+    if ((f[e.x+1][e.y-1] != 0) && (f[e.x+1][e.y-1] != 3)) ret = 1;
+    if ((f[e.x+2][e.y-1] != 0) && (f[e.x+2][e.y-1] != 3)) ret = 1;
+
+    if ((f[e.x-1][e.y] != 0) && (f[e.x-1][e.y] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+1] != 0) && (f[e.x-1][e.y+1] != 3)) ret = 1;
+    if ((f[e.x-1][e.y+2] != 0) && (f[e.x-1][e.y+2] != 3)) ret = 1;
+
+    if ((f[e.x-1][e.y-1] != 0) && (f[e.x-1][e.y-1] != 3)) ret = 1;
+    return ret;
+}
+
+// 0 if ok, 1 if not
+
+int checkMoveDown(Entity e){
+    int ret = 0;
+    if ((f[e.x][e.y-1] != 0) && (f[e.x][e.y-1] != 3)) ret = 1;
+    if ((f[e.x+1][e.y-1] != 0) && (f[e.x+1][e.y-1] != 3)) ret = 1;
+    if ((f[e.x+2][e.y-1] != 0) && (f[e.x+2][e.y-1] != 3)) ret = 1;
+    if ((f[e.x+2][e.y-1] != 0) && (f[e.x+3][e.y-1] != 3)) ret = 1;
+    return ret;
+}
 
 void init(){
 
@@ -73,7 +136,26 @@ void print(Field f){
 }
 
 int main(){
+
+    Entity e1;
+    e1.x = 10;
+    e1.y = 10;
+
+    Entity e2;
+    e2.x = 6;
+    e2.y = 6;
+
     init();
+
+    entityMod(e1, 1);
+    entityMod(e2, 1);
+
     print(f);
+
+    printf("Move Up : %d\n", checkMoveUp(e1));
+    printf("Move Upper Left : %d\n", checkMoveUpperLeft(e1));
+    printf("Move Left : %d\n", checkMoveLeft(e1));
+    printf("Move Lower Left : %d\n", checkMoveLowerLeft(e1));
+    printf("Move Down : %d\n", checkMoveDown(e1));
     return 0;
 }
