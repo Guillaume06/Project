30deeb2fa9e8962c9315348cb6afe872d548f1dc
guillaume <guillaume_fdr@orange.fr>
2016-09-17 14:20:44 +0200

    Field begenning

--- src/Field.c
+++ src/Field.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main(){
+typedef int Field[512][128];
 
-    printf("Hello world\n");
+Field f;
+
+void init(){
+
+    for (int i = 0; i<512; i++){
+        for (int j = 0; j< 128; j++){
+            f[i][j] = 0;
+        }
+    }
+    for (int i=0; i<= 60 ; i++){
+        f[0][i] = 2;
+    }
+    for (int i=69; i<= 128 ; i++){
+            f[0][i] = 2;
+    }
+}
+
+void print(Field f){
+    for (int i = 0; i< 512; i++){
+        for (int j = 0; j< 128; j++){
+            printf("%d", f[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    init();
+    print(f);
     return 0;
 }
