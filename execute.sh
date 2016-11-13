f2af8c11d94679c5f3b70b5883398c1e325c8721
guillaume <guillaume_fdr@orange.fr>
2016-11-13 21:45:44 +0100

    For demo presets

--- execute.sh
+++ execute.sh
@@ -2,4 +2,4 @@
 #  @author : GONNIN THIBAUT, FILIOL DE RAIMOND-MICHEL Guillaume
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $ABSOLUTE_PATH/bin
-./Field -e1 -t1 -p4 -m
+./Field -e2 -t1 -p4 -m
